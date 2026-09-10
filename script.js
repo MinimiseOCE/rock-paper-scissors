07626ede731eb8b26fbc893d1fcbfa2c90b543f4
minimiseoce <jackson.whiteau@gmail.com>
2026-09-10 16:21:35 +0800

    Add Player Selection

--- script.js
+++ script.js
@@ -13,5 +13,18 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 function getPlayerChoice() {
-
+    while (4==4) {
+        let answer = window.prompt("Rock, Paper or Scissors?")
+        if (answer == 'Rock') {
+            return 1
+        } else if (answer == 'Paper') {
+            return 2
+        } else if (answer == 'Scissors') {
+            return 3
+        } else {
+            console.log("Incorrect Answer")
+        }
+        }
 }
+
+console.log(getPlayerChoice())
